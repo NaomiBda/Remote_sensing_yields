73c66de2143a2175561cf9d0d4199b31d4f2e653
NaomiBda <38361396+NaomiBda@users.noreply.github.com>
2020-03-12 12:40:43 +0000

    update file

--- lecture_shapefiles.py
+++ lecture_shapefiles.py
@@ -13,25 +13,37 @@ class Shapefile(object):
         self.path=path
         self.Files=Files
         
+    def load_shapefile(self):
+        """
+        crée une liste de geodataframes qui correspondent au shapefiles en entrée
         
-    def  change_projection(self,File,projection):
+        """
+        self.SHP=[]
+        for k in range (len(self.Files)):
+            test=gpd.read_file(self.path+self.Files[k])
+            self.SHP+=[test]
+
+        
+    def  change_projection(self,projection):
         """
         mets tous les shapefiles en projection 4326
         """
-        file = gpd.read_file(self.path+File)
+        self.load_shapefile()
         idproj = projection[5:] #id of the projection_ Ex:4326
-        newfile = file.to_crs(epsg=idproj)
-        return(newfile)
+        for k in range(len(self.SHP)):
+            file=self.SHP[k]
+            newfile = file.to_crs(epsg=idproj)
+            self.SHP[k]=newfile
 
-    def affiche_shapefiles(self):
+    def plot_shapefiles(self):
         """
         affiche la superposition des shapefiles
         """
         colors=['#ffff00','#6699cc','#791cf8','#FF69B4','#00ff7f','#ff0000','#ff7f50']
         fig = plt.figure() 
+        self.change_projection('epsg:4326') #met tout sur la projection 4326
         for j in range(len(self.Files)):
-            formes=self.Files[j]
-            self.test = self.change_projection(formes,'epsg:4326') #met tout sur la projection 4326
+            self.test =  self.SHP[j]
             color=colors[j]
             
             for k in range(len(self.test.index)):
@@ -41,7 +53,6 @@ class Shapefile(object):
                 ax = fig.gca() 
                 ax.add_patch(PolygonPatch(poly, fc=color, ec=color, alpha=0.5, zorder=2 ))
                 ax.axis('scaled')
-        
         plt.show()
         
 
@@ -57,6 +68,7 @@ Files= ["whole-plot.shp","Yield9Plots_4326.shp","Faidherbia.shp","Shelter.shp",]
 
 
 A=Shapefile(path,Files)
-A.affiche_shapefiles()
+A.plot_shapefiles()
+
 
 
