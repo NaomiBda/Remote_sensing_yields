d87ec53e4b02c665be937605cf281380a0bed067
NaomiBda <38361396+NaomiBda@users.noreply.github.com>
2020-08-11 11:01:38 +0200

    Update mergebands.py

--- mergebands.py
+++ mergebands.py
@@ -8,38 +8,37 @@ Created on Wed Aug  5 18:31:39 2020
 
 
 import rasterio as rio
-from rasterio.merge import merge
-import os
 
-def fusionner(path,liste_raster):
-    os.chdir(path)
-    src_list=[]
-    for raster in liste_raster:
-        src=rio.open(path+raster)
-        src_list+=[src]
-    mosaic, out_trans = merge(src_list)
-    # Copy the metadata
-    out_meta = src.meta.copy()
 
-# Update the metadata
-    out_meta.update({"driver": "GTiff",
-                 "height": mosaic.shape[1],
-                 "width": mosaic.shape[2],
-                 "transform": out_trans,
-                 "crs": "+proj=utm +zone=35 +ellps=GRS80 +units=m +no_defs "
-                 }
-                )
-    out_fp=path+"Planet.tif"
-    with rio.open(out_fp, "w", **out_meta) as dest:
-        dest.write(mosaic)
+def fusionner (liste,path):
 
+    # Read metadata of first file
+    with rio.open(path+liste[0]) as src0:
+        meta = src0.meta
     
+    # Update meta to reflect the number of layers
+    meta.update(count = len(liste))
+    
+    # Read each layer and write it to stack
+    with rio.open(path+'stack.tif', 'w', **meta) as dst:
+        for id, layer in enumerate(liste, start=1):
+            with rio.open(path+layer) as src1:
+                dst.write_band(id, src1.read(1))
 
 
 
 
 if __name__=="__main__":
-    path="/Volumes/My Passport 1/TempNaomi/Donnees/Drone/2018/Niakhar/2018_10_08/plot 2018/NDVI_norm_0/valuesModel/"
-    liste=["Planet2018b1.tif","Planet2018b2.tif","Planet2018b3.tif","Planet2018b4.tif"]
-    fusionner(path,liste)
+    #2018
+    # path="/Volumes/My Passport 1/TempNaomi/Donnees/Drone/2018/Niakhar/2018_10_08/plot 2018/NDVI_norm_0/valuesModel/"
+    # liste=["Planet2018b1.tif","Planet2018b2.tif","Planet2018b3.tif","Planet2018b4.tif"]
+    # fusionner(liste,path)
+    
+    #2019
+    path="/Volumes/My Passport 1/TempNaomi/Donnees/Drone/2019/Niakhar/19-09-05/plot 2019/NDVI_norm_0/valuesModel/"
+    liste=["planetaligneb1.tif","planetaligneb2.tif","planetaligneb3.tif","planetaligneb1.tif"]
+    fusionner(liste,path)
+    
+    
+    
     
