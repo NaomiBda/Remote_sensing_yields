33e43c63b1be2d6d0ae3271eecba56a45ebcc80d
NaomiBda <38361396+NaomiBda@users.noreply.github.com>
2020-08-13 10:43:31 +0200

    Update RasterToArray.py

--- RasterToArray.py
+++ RasterToArray.py
@@ -8,6 +8,7 @@ Created on Wed Aug 12 14:24:13 2020
 
 import rasterio as rio
 import numpy as np 
+import pandas as pd
 
 def noNa(path_drone):
     """
@@ -43,8 +44,8 @@ def transform(path,raster,path_drone):
     for i in range(n):
         for j in range(m):
             if [i,j] not in listeNa:
-                matrice[k,0]=i
-                matrice[k,1]=j
+                matrice[k,0]=int(i)
+                matrice[k,1]=int(j)
             
                 for bands in range(b):
                 
@@ -58,20 +59,37 @@ def transform(path,raster,path_drone):
 
     
     
-def write_csv(matrice,entetes):
+def write_csv(path,matrice,header,formt):
     """
     ecrit un csv à partir dune matrice et dune entete (une liste de deux listes de taille de la matrice)
+    path = path where to load the file
+    matrice : array 
+    header = str : header
+    formt : format (int, float etc.. )
     """
     
+    np.savetxt(path+"matrice_indices.csv", matrice, delimiter=",",fmt=formt,header=header)
     
 
     
 if __name__=='__main__':
-    path='/Volumes/My Passport 1/TempNaomi/Donnees/Planet/plot 2018/'
-    path_drone='/Volumes/My Passport 1/TempNaomi/Donnees/Drone/2018/Niakhar/2018_10_08/plot 2018/NDVI_norm_0/valuesModel/2018reprojetedrone.tif'
-    raster='allindexesPlanet2018.tif'
+    #2018
+   # path='/Volumes/My Passport 1/TempNaomi/Donnees/Planet/plot 2018/'
+    #path_drone='/Volumes/My Passport 1/TempNaomi/Donnees/Drone/2018/Niakhar/2018_10_08/plot 2018/NDVI_norm_0/valuesModel/2018reprojetedrone.tif'
+    #raster='allindexesPlanet2018.tif'
+    
+    #2019
+    path='/Volumes/My Passport 1/TempNaomi/Donnees/Planet/plot 2019/'
+    path_drone='/Volumes/My Passport 1/TempNaomi/Donnees/Drone/2019/Niakhar/19-09-05/plot 2019/NDVI_norm_0/valuesModel/drone2019projete.tif'
+    raster='Planetallindexes2019.tif'
+    
+    
+    #code
     matrice=transform(path,raster,path_drone)
+    header='pixel line index,pixel column index,NDVI value, NDVI norm value,EVI value, GNDVI value,Excess green value, MSVAI value'
+    formt='%d','%d','%1.4f','%1.4f','%1.4f','%1.4f','%d','%1.4f' # '%d' means only integeer values , '%1.4f' means decimal with 4 figures
     print(matrice)
+    write_csv(path,matrice,header,formt)
     #write_csv(matrice,entete)
     
     
