c4eb52be3da84a03107e8da7379af06ccbe5a040
NaomiBda <38361396+NaomiBda@users.noreply.github.com>
2020-09-08 23:34:29 +0200

    Update reglinsat.py

--- reglinsat.py
+++ reglinsat.py
@@ -95,7 +95,7 @@ class Data(object):
            
        show_regression(X,Y,Xvalue,Yvalue)
        
-    def reglin_multiple(self,Xvalues,Yvalue):
+    def reglin_multiple(self,Xvalues,Yvalue,pathfinal):
         """
         Xvalues:list of labels
         Yvalue : str : Y label
@@ -109,21 +109,40 @@ class Data(object):
         lm2018=LinearRegression()
         reg2018=lm2018.fit(X2018,Y2018)
         a2018=reg2018.coef_
-        b=reg2018.intercept_
+        b2018=reg2018.intercept_
         Y_pred2018=lm2018.predict(X2018)
         r218=reg2018.score(X2018,Y2018)
-        print(a2018)
+        rmse2018=mean_squared_error(Y2018, Y_pred2018)
+        moy18=np.mean(Y2018)
+        rrmse2018=rmse2018/moy18
           
         lm2019=LinearRegression()
         reg2019=lm2019.fit(X2019,Y2019)
         a2019=reg2019.coef_
-        b=reg2019.intercept_
+        b2019=reg2019.intercept_
         Y_pred2019=lm2019.predict(X2019)
         r219=reg2019.score(X2019,Y2019)
+        rmse2019=mean_squared_error(Y2019, Y_pred2019)
+        moy19=np.mean(Y2019)
+        rrmse2019=rmse2019/moy19
         
-        print(a2019)
-        print(r218,r219)
         
+        nombre_indices=len(Xvalues)
+        
+        with open(pathfinal+'RegressionMultiples_modif.csv', 'w',newline='') as csvfile:
+            fieldnames = ['coefficient','2018','2019']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for indices in range(nombre_indices):
+                row={'coefficient':Xvalues[indices],'2018':str(round(a2018[indices],3)),'2019':str(round(a2019[indices],3))}
+                writer.writerow(row)
+            row1={'coefficient':'Terme constant','2018':str(round(b2018,2)),'2019':str(round(b2019,2))}
+            row2={'coefficient':'r2','2018':str(round(r218,3)),'2019':str(round(r219,3))}
+            row3={'coefficient':'Rrmse','2018':str(round(rrmse2018,3)),'2019':str(round(rrmse2019,3))}
+            writer.writerow(row1)
+            writer.writerow(row2)
+            writer.writerow(row3)
+
        
     def write_csv_glob(self,Xvalues,Yvalue,pathfinal):
         """
@@ -131,14 +150,14 @@ class Data(object):
         Yvalue:str: Y label
         """
         with open(pathfinal+'RegressionSimples.csv', 'w',newline='') as csvfile:
-            fieldnames = ['indices','r2 2018','r2 2019','Rrmse 2018','Rrmse 2019']
+            fieldnames = ['indices','r2 2018','r2 2019','Rrmse 2018','Rrmse 2019','coeff dir 2018','coeff dir 2019']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             
             for Xvalue in Xvalues:
                 self.reglin(Xvalue,Yvalue)
 
-                row={'indices':str(Xvalue),'r2 2018':str(self.r218),'r2 2019':str(self.r219),'Rrmse 2018':str(self.relat_rmse18),'Rrmse 2019': self.relat_rmse19}
+                row={'indices':str(Xvalue),'r2 2018':str(round(self.r218,5)),'r2 2019':str(round(self.r219,5)),'Rrmse 2018':str(round(self.relat_rmse18,5)),'Rrmse 2019': str(round(self.relat_rmse19,5)),'coeff dir 2018':str(round(self.a18,2)),'coeff dir 2019':str(round(self.a19,2))}
                
                 writer.writerow(row)
                 
@@ -149,13 +168,13 @@ if __name__ == '__main__':
     path2018="/Volumes/My Passport 1/TempNaomi/Donnees/Planet/plot 2018/matrice_indices_20181004.csv"
     path2019="/Volumes/My Passport 1/TempNaomi/Donnees/Planet/plot 2019/matrice_indices_2019.csv"
     pathfinal="/Volumes/My Passport 1/TempNaomi/Donnees/Planet/"
-    Xvalues= ['NDVI value',' NDVI norm value','EVI value',' GNDVI value','Excess green value',' MSVAI value']
+    Xvalues= [' NDVI norm value',' GNDVI value',' MSVAI value']
     Yvalue='yield estimation value(kg.m-1)'
     Xvalue =' GNDVI value'
     A=Data(path2018,path2019)
-    A.reglin_multiple(Xvalues,Yvalue)
+    A.reglin_multiple(Xvalues,Yvalue,pathfinal)
     #A.write_csv_glob(Xvalues,Yvalue,pathfinal)
-    #print(A.r218,A.r219)
+    #print(A.a19,A.b19)
     #A.showreg(Xvalue,Yvalue,'2019')
     
         
